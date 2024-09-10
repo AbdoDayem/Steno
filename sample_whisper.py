ba1820ea86f05c63ea16fe657d9da78e26f0241e
Giridhar Srikanth <srigiridhar27@gmail.com>
2024-09-10 14:57:10 -0400

    added 10 min speech

--- sample_whisper.py
+++ sample_whisper.py
@@ -1,5 +1,10 @@
 import whisper
+import time
+
+start_time = time.time()
 
 model = whisper.load_model("base")
-result = model.transcribe("sample_audio/rockdj.mp3")
+result = model.transcribe("sample_audio/sample_speech.mp3")
 print(result["text"])
+
+print("--- %s seconds ---" % (time.time() - start_time))
