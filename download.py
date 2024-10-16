96f51a8071994b1f29b0cdfdf2dba92cda1e774c
Abdullah Ahmed <abdall.dayem@gmail.com>
2024-10-16 15:07:36 -0400

    non-elastic based solution

---
+++ download.py
@@ -0,0 +1,51 @@
+import datetime
+import pprint
+from elasticsearch import Elasticsearch
+import urllib.request
+
+# Create the client instance
+# ELASTIC_PASSWORD = "TCG_7GzN6ZFKej4Fut-m"
+
+# # Create the client instance
+# client: Elasticsearch = Elasticsearch(
+#     "https://localhost:9200",
+#     ca_certs="./certs/ca-cert.pem",
+#     basic_auth=("elastic", ELASTIC_PASSWORD)
+# )
+
+file = open('./html.txt', 'r')
+doc_text = file.readlines()
+file.close()
+
+matches = [match for match in doc_text if "</audio>" in match]
+lines = []
+for match in matches:
+    lines += match.split()
+sources = [line for line in lines if ('src' in line)]
+
+source = sources[0].replace('src=', '').replace('\"', '')
+
+if 'https' not in source: source = 'https:' + source
+
+urllib.request.urlretrieve(source, "./media/audiofile.mp3")
+
+
+# doc = {
+#     'text': doc_text
+# }
+
+# #client.indices.create(index="test-index")
+
+# res = client.index(index="test-index", id=1, document=doc)
+
+# # resp = client.search(
+# #     index="test-index",
+# #     query={"match": {"text": {"query": "<audio"}}}
+    
+# # )
+
+
+# pprint.pp(resp["hits"]["hits"])
+
+# #client.delete(index="test-index", id=1)
+# #client.indices.delete(index="test-index")
