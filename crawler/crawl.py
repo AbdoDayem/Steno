406cda5a3339244601efbe7e56604f32e0faed4c
Abdullah Ahmed <abdall.dayem@gmail.com>
2024-10-24 14:56:24 -0400

    crawl.py runs forever

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,12 +1,11 @@
 import requests
 from subprocess import call
 
-
 old_url = ""
 current_url = requests.get('http://localhost:5000/url').content.decode("UTF-8")
-print(current_url)
-while(old_url == current_url):
+while(True):
+    while(old_url == current_url):
+        old_url = current_url
+        current_url = requests.get('http://localhost:5000/url').content.decode("UTF-8")
+    rc = call("./crawl " + current_url, shell=True)
     old_url = current_url
-    current_url = requests.get('http://localhost:5000/url').content.decode("UTF-8")
-
-rc = call("./crawl " + current_url, shell=True)
