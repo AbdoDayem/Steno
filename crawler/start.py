c8c02f640b1be10edcb16d7a4aed6beacb1c6b42
Giridhar Srikanth <srigiridhar27@gmail.com>
2024-10-29 15:20:13 -0400

    dockerizing it all

--- crawler/start.py
+++ crawler/start.py
@@ -1,10 +1,12 @@
 import subprocess
+import time
 
 def run_script(script_name):
     subprocess.Popen(["python", script_name])
 
 if __name__ == "__main__":
-    scripts = ["transcription/run.py", "backend/app.py"]
+    scripts = ["../transcription/run.py", "../backend/app.py", "crawl.py"]
     
     for script in scripts:
         run_script(script)
+        time.sleep(0.5)
