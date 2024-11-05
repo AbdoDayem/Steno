cdf559974eb3b5e8593a10faf42453e0f4c0e7f3
Ryan Tussing <ryan.m.tussing@gmail.com>
2024-11-05 17:25:57 -0500

    Linking stuff together

--- crawler/crawl.py
+++ crawler/crawl.py
@@ -1,11 +1,13 @@
 import requests
 from subprocess import call
+import time
 
 old_url = ""
-current_url = requests.get('http://0.0.0.0:5001/url').content.decode("UTF-8")
+current_url = requests.get('http://backend:5001/url').content.decode("UTF-8")
 while(True):
     while(old_url == current_url):
+        time.sleep(1)
         old_url = current_url
-        current_url = requests.get('http://0.0.0.0:5001/url').content.decode("UTF-8")
+        current_url = requests.get('http://backend:5001/url').content.decode("UTF-8")
     rc = call("./crawl " + current_url, shell=True)
     old_url = current_url
